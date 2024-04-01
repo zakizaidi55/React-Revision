1917d51ff1e0688d4051c816b58867b4492ee2e6
zakizaidi55 <zakizaidi55@gmail.com>
2024-04-01 14:42:50 +0530

    appwrite authentication service

---
+++ 12megaBlogAppwrite/src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import conf from '../config/conf';
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appWriteProjectId);
+        
+        this.account = new Account(this.client);
+
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount) {
+                // call another method
+                return this.login({email, password});
+            }
+
+            else {
+                return userAccount;
+            }
+        } catch(e) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch(error) {
+            throw error;
+        }
+        
+    }
+
+    async getCurrentUser () {
+        try {
+            return await this.account.get();
+        } catch(error) {
+            throw error;
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch(error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
+ 
