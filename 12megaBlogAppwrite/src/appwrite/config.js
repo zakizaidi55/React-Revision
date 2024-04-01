20c969517aac56934beec16c94b296fdc09357d5
zakizaidi55 <zakizaidi55@gmail.com>
2024-04-01 23:47:04 +0530

    config file for file related

---
+++ 12megaBlogAppwrite/src/appwrite/config.js
@@ -0,0 +1,140 @@
+import conf from '../config/conf';
+import { Client, ID, Databases, Storage, Query, AppwriteException } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.appWriteProjectId);
+            this.databases = new Databases(this.client);
+            this.bucket = new Storage(this.client);
+    
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}) {
+        try {
+            return await this.databases.createDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service create::", error)
+            throw error;
+        }
+    }
+
+    async updatePost(slug, {title,content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service update::", error)
+            throw error;
+        }
+    }
+
+    async deletePost({slug}) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+            )
+
+            return true;
+        } catch (error) {
+            console.log("Appwrite service delete ::", error);
+            throw error;
+            return false;
+        }
+    }
+
+    async getPost({slug}) {
+        try {
+            return await this.databases.getDocument(
+                conf.appWriteDatabaseId,
+                conf.appWriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("Appwrite service getpost::", error)
+            throw error;
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]) {
+        try { 
+            return await this.databases.listDocuments(
+                conf.appWriteDatabaseId, 
+                conf.appWriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite service getpost::", error)
+            throw error;
+            return false;
+        }
+    }
+
+    // upload file
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appWriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("Appwrite service uploadfile::", error)
+            throw error;
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appWriteBucketId,
+                fileId,
+            )
+
+            return true;
+        } catch (error) {
+            console.log("Appwrite service deleleFile::", error)
+            throw error;
+        }
+    }
+
+    async getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appWriteBucketId,
+            fileId,
+        )
+    }
+}
+
+
+const service = new Service();
+export default service
