84bf2014cb058aaac5ef9bef55563d22dce84dc3
zakizaidi55 <zakizaidi55@gmail.com>
2024-04-02 17:13:48 +0530

    setup react redux toolkit for authentication

--- 12megaBlogAppwrite/src/App.jsx
+++ 12megaBlogAppwrite/src/App.jsx
@@ -1,11 +1,41 @@
 
+import { useState, useEffect } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}));
+      }
+
+      else {
+        dispatch(logout())
+      }
+
+    })
+    .finally(() => setLoading(false));
+  },[])
+
   return (
     <>
-      <h1>App with appwrite</h1>
+      !loading ? (
+        <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+          <Header/>
+          <main>
+            {/* todo <Outlet/> */}
+          </main>
+          <Footer/>
+        </div>
+      ) : (null)
     </>
   )
 }
